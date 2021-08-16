1e1b9fd2fe8edcd227e731b15aeb0b9520c171e8
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-16 18:37:26 +0800

    [Counter][Juan Carlos Salvador] Feat: Implemented Counter with no functionality

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Counter(){
+
+    return (
+        <div>
+            <button>+</button>
+            <span>1</span>
+            <button>-</button>
+        </div>
+    );
+}
+
+
+export default Counter;
