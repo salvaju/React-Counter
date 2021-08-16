9a56d7e453c110f867faee809af5a2a8bf18f4a9
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-16 18:41:05 +0800

    [Counter][Juan Carlos Salvador] Feat: Implemented Counter with functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter(){
+    const [number, setNumber] = useState(0);
 
     return (
         <div>
-            <button>+</button>
-            <span>1</span>
-            <button>-</button>
+            <button onClick = {increase}>+</button>
+            <span>{number}</span>
+            <button onClick = {decrease}>-</button>
         </div>
     );
+
+    function increase() {
+        setNumber(number + 1);
+    }
+
+    function decrease() {
+        setNumber(number - 1);
+    }
 }
 
 
